72c86300924f83856c60be7e25d8a0734e62988a
rabiademirbag <rddemirbagg@gmail.com>
2025-07-14 22:42:15 +0300

    Proje dosyası ekle.

---
+++ PatikaFirstDb/Data/PatikaFirstDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PatikaFirstDb.Data.Entities;
+
+namespace PatikaFirstDb.Data
+{
+    public class PatikaFirstDbContext:DbContext
+    {
+
+        //connection string program.cs den geliyor
+        public PatikaFirstDbContext(DbContextOptions<PatikaFirstDbContext> options) : base(options) { }
+
+
+        public DbSet<Game>Games { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Movie>(entity =>
+            {
+                entity.ToTable("Movies");
+            });
+
+            modelBuilder.Entity<Game>(entity =>
+            {
+                entity.ToTable("Games");
+            });
+        }
+    }
+}
